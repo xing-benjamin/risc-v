adbb8c77d3cf4b5f1335ec5e145c3d72c99bd813
emvick <37252095+emvick@users.noreply.github.com>
2022-10-18 23:19:58 -0700

    Update alu_tb.sv
    Add and, or, and xor

--- core/tb/alu_tb.sv
+++ core/tb/alu_tb.sv
@@ -42,18 +42,18 @@ module alu_tb;
     int case_sel;
     always begin
         #($urandom_range(10, 0));
-        case_sel = $urandom_range(6, 0);
+        case_sel = $urandom_range(9, 0);
         case (case_sel)
             0: alu_op <= 4'b0000;
             1: alu_op <= 4'b0001;
             2: alu_op <= 4'b0010;
             3: alu_op <= 4'b0100;
             4: alu_op <= 4'b0110;
-            // 5: alu_op <= 4'b1000; TODO EMILY: uncomment XOR
+            5: alu_op <= 4'b1000;
             6: alu_op <= 4'b1010;
             7: alu_op <= 4'b1011;
-            // 8: alu_op <= 4'b1100; TODO EMILY: uncomment OR
-            // 9: alu_op <= 4'b1110; TODO EMILY: uncomment AND
+            8: alu_op <= 4'b1100;
+            9: alu_op <= 4'b1110;
         endcase
     end
 
@@ -62,13 +62,13 @@ module alu_tb;
     always @(alu_op, in0, in1) begin
         #0.1;
         exp_output = get_expected_output(alu_op, in0, in1);
-        alu_trace_str = $sformatf("alu_op = %4b, in0 = %8h, in1 = %8h, out = %8h, exp_out = %8h", 
+        alu_trace_str = $sformatf("alu_op = %4b, in0 = %8h, in1 = %8h, out = %8h, exp_out = %8h",
                         alu_op, in0, in1, out, exp_output);
         `CHECK_RESULT(alu_trace_str, exp_output === out)
     end
 
     function logic [N_BITS-1:0] get_expected_output(logic [3:0] alu_op,
-                                                    logic [N_BITS-1:0] in0, 
+                                                    logic [N_BITS-1:0] in0,
                                                     logic [N_BITS-1:0] in1);
         case (alu_op)
             4'b0000: return in0 + in1;
