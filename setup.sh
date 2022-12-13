198bf1ffc53ead0cb28dc67ec1154f49b7b05d94
Ben Xing <benjamin.y.xing@gmail.com>
2022-12-12 22:53:40 -0800

    Export environment vars for subdirectories

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 export ROOTDIR=`git rev-parse --show-toplevel`
+export COMMON_DIR=$ROOTDIR/common
+export DESIGN_LIB=$ROOTDIR/design_lib
+export CORE_DIR=$ROOTDIR/core
 export PATH=$PATH:$ROOTDIR/scripts
